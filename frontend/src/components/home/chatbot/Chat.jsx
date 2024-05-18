09738ebba26638c2ffaf89f8f5a756c8d8bfcdb4
Denis Rocha <dns_rochaa@hotmail.com>
2024-05-18 19:34:29 -0300

    finish-deploy

--- frontend/src/components/home/chatbot/Chat.jsx
+++ frontend/src/components/home/chatbot/Chat.jsx
@@ -1,17 +1,32 @@
+import React, { useState } from "react";
 import Chatbot from "react-chatbot-kit";
 import "react-chatbot-kit/build/main.css";
+import { FaRobot } from "react-icons/fa"; // Importando ícone de robô
 import config from "./config";
 import MessageParser from "./MessageParser";
 import ActionProvider from "./ActionProvider";
 
 const Chat = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleChatbot = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div>
-      <Chatbot
-        config={config}
-        messageParser={MessageParser}
-        actionProvider={ActionProvider}
-      />
+    <div className='m-5'>
+      {!isOpen && (
+        <button onClick={toggleChatbot} className="flex items-center justify-center p-2 bg-blue-500 text-white rounded-full">
+          <FaRobot className="text-5xl" /> {/* Ícone de robô */}
+        </button>
+      )}
+      {isOpen && (
+        <Chatbot
+          config={config(toggleChatbot)}
+          messageParser={MessageParser}
+          actionProvider={ActionProvider}
+        />
+      )}
     </div>
   );
 };
--- frontend/src/components/home/chatbot/config.js
+++ frontend/src/components/home/chatbot/config.js
@@ -1,15 +1,22 @@
-import React from 'react';
-import { createChatBotMessage } from 'react-chatbot-kit';
-import BotAvatar from './BotAvatar';
+import React from "react";
+import { createChatBotMessage } from "react-chatbot-kit";
+import BotAvatar from "./BotAvatar";
+import { AiOutlineClose } from "react-icons/ai"; // Importando ícone de fechar
 
-const botName = 'Rebot';
+const botName = "Rebot";
 
-const config = {
+const config = (onClose) => ({
   initialMessages: [createChatBotMessage(`Hi! I'm ${botName}`)],
   botName: botName,
   customComponents: {
-   botAvatar:(props) => <BotAvatar {...props} />,
-  }
-}
+    botAvatar: (props) => <BotAvatar {...props} />,
+    header: () => (
+      <div className="bg-sky-800 text-white px-4 py-1 flex items-center justify-between">
+        Conversa com o Rebot
+        <AiOutlineClose className="text-2xl cursor-pointer" onClick={onClose} /> {/* Ícone de fechar */}
+      </div>
+    ),
+  },
+});
 
 export default config;
